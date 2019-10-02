523f2f7ea16d355f067c5fa2a823684764fcd958
rajdas <mail.rajdas@gmail.com>
2019-10-03 03:26:43 +0530

    testing
    Signed-off-by: rajdas <mail.rajdas@gmail.com>

--- scripts/version_maker.sh
+++ scripts/version_maker.sh
@@ -1,8 +1,7 @@
 #! /bin/bash 
 
 # Retrive the last pushed commit from the repo
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-last_commit_hash=`git merge-base $branch HEAD`
+last_commit_hash=`git merge-base testing HEAD`
 echo "Last commit hash: $last_commit_hash"
 
 # This function is used to parse the yaml file.
