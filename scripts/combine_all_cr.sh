60fc34de64cbf23ffa3360915b4685fd2d71df1c
shubhamchaudhary <shubham.chaudhary@mayadata.io>
2019-10-03 18:10:57 +0530

    Add[CI]: combine experiments CRwq!
    Signed-off-by: shubhamchaudhary <shubham.chaudhary@mayadata.io>

--- scripts/combine_all_cr.sh
+++ scripts/combine_all_cr.sh
@@ -1,29 +1,19 @@
-directories=$(find ./charts -maxdepth 1 -type d)
+directories=$(find ../charts -maxdepth 1 -mindepth 1 -type d)
 for directory in $directories
 do
-	if [ $directory == "./charts" ]
-        then 
-		continue
-        fi
-	subDirectories=$(find $directory -maxdepth 1 -type d)
+	subDirectories=$(find $directory -maxdepth 1 -mindepth 1 -type d)
+	echo -e "---\n" > $directory/experiments.yaml
 	for subDirectory in $subDirectories
 	do     
-	       if [ $subDirectory == $directory ] 
-	       then 
-		       continue
-	       fi
-               if test -f "$subDirectory/experiment.yaml"; then	       
-	       if test -f "$directory/experiments.yaml"; then
+
+               if test -f "$subDirectory/experiment.yaml"; then
 	          cat $subDirectory/experiment.yaml >> $directory/experiments.yaml
-	      else
-	          cat $subDirectory/experiment.yaml > $directory/experiments.yaml
-	      fi
-	      echo -e "\n---\n" >> $directory/experiments.yaml                  		  
+	          echo -e "\n---\n" >> $directory/experiments.yaml                  		  
 	       fi
         done
 done
 echo "validating combine charts for generic"
-cat ./charts/generic/experiments.yaml
+cat ../charts/generic/experiments.yaml
 echo "validating combine charts for openebs"
-cat ./charts/openebs/experiments.yaml
+cat ../charts/openebs/experiments.yaml
 
