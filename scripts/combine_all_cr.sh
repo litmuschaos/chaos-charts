7a0dac5afab7afc614612191bc7e3277c71e49e6
shubhamchaudhary <shubham.chaudhary@mayadata.io>
2019-10-03 14:41:53 +0530

    Add[CI]: Combine Experiments CR
    Signed-off-by: shubhamchaudhary <shubham.chaudhary@mayadata.io>

--- scripts/combine_all_cr.sh
+++ scripts/combine_all_cr.sh
@@ -1,12 +1,12 @@
-directories=$(ls -l ../charts/ | grep ^d | awk '{print $9}')
+directories=$(ls -l ./charts/ | grep ^d | awk '{print $9}')
 for directory in $directories
 do
-	echo -e "---\n" > ../charts/$directory/experiments.yaml
-	subDirectories=$(ls -l ../charts/$directory | grep ^d | awk '{print $9}')
+	echo -e "---\n" > ./charts/$directory/experiments.yaml
+	subDirectories=$(ls -l ./charts/$directory | grep ^d | awk '{print $9}')
 	for subDirectory in $subDirectories
 	do 
-	cat ../charts/$directory/$subDirectory/experiment.yaml >> ../charts/$directory/experiments.yaml
-	echo -e "\n---\n" >> ../charts/$directory/experiments.yaml
+	cat ./charts/$directory/$subDirectory/experiment.yaml >> ./charts/$directory/experiments.yaml
+	echo -e "\n---\n" >> ./charts/$directory/experiments.yaml
         done
 done
 
