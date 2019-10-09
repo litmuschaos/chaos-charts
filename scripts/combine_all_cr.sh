d9c7d1710b217ba4a694ca61e8b28194518936a5
Raj Das <mail.rajdas@gmail.com>
2019-10-09 17:13:25 +0530

    adding minor changes
    Signed-off-by: Raj Das <mail.rajdas@gmail.com>

--- scripts/combine_all_cr.sh
+++ scripts/combine_all_cr.sh
@@ -2,11 +2,11 @@ directories=$(find ./charts -maxdepth 1 -mindepth 1 -type d)
 for directory in $directories
 do
 	subDirectories=$(find $directory -maxdepth 1 -mindepth 1 -type d)
-	echo        done
+	echo -e "---\n" > $directory/experiments.yaml
 	for subDirectory in $subDirectories
 	do     
 
-			if test -f "$subDirectory/experiment.yaml"; then
+               if test -f "$subDirectory/experiment.yaml"; then
 	          cat $subDirectory/experiment.yaml >> $directory/experiments.yaml
 	          echo -e "\n---\n" >> $directory/experiments.yaml                  		  
 	       fi
@@ -16,4 +16,3 @@ echo "validating combine charts for generic"
 cat ./charts/generic/experiments.yaml
 echo "validating combine charts for openebs"
 cat ./charts/openebs/experiments.yaml
-
