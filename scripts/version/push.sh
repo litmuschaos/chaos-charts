8f665be0c99da50cb0ccd2e31beca0b7f934f8e0
udit <udit.gaurav@mayadata.io>
2020-12-23 05:13:46 +0530

    # This is a combination of 2 commits. # This is the 1st commit message:
    chore(github_actions): Migrate travis to github actions for chaos-charts
    
    Signed-off-by: udit <udit.gaurav@mayadata.io>
    
    # The commit message #2 will be skipped:
    
    # Avoid running push for forked repo
    #
    # Signed-off-by: udit <udit.gaurav@mayadata.io>

--- scripts/version/push.sh
+++ scripts/version/push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+  git config --global user.email "litmusbotci@gmail.com"
+  git config --global user.name "Litmus[BOT]"
   git remote set-url origin  https://${GITHUB_TOKEN}@github.com/litmuschaos/community-charts.git > /dev/null 2>&1
 }
 
@@ -9,15 +9,10 @@ commit_updated_changes() {
   git checkout master
   git status
   git add .
-  git commit --message " $TRAVIS_BUILD_NUMBER: version upgraded for chaos-charts"
+  git commit --message " $GITHUB_RUN_ID: version upgraded for chaos-charts"
   git status
-}
-
-upload_files() {
-  git remote -v
-  git push origin master
 }
 
 setup_git
 commit_updated_changes
-upload_files
+
