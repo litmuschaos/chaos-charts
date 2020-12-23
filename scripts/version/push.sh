467dbc8e4249197e9f9378a00a522c56e40fcfde
Udit Gaurav <35391335+uditgaurav@users.noreply.github.com>
2020-12-23 18:22:04 +0530

    chore(github_actions): Migrate travis to github actions for chaos-charts (#403)
    chore(github_actions): Migrate travis to github actions for chaos-charts
    
    Signed-off-by: udit <udit.gaurav@mayadata.io>

--- scripts/version/push.sh
+++ scripts/version/push.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-  git remote set-url origin  https://${GITHUB_TOKEN}@github.com/litmuschaos/community-charts.git > /dev/null 2>&1
+  git config --global user.email "litmusbotci@gmail.com"
+  git config --global user.name "litmus[bot]"
 }
 
 commit_updated_changes() {
   git checkout master
   git status
   git add .
-  git commit --message " $TRAVIS_BUILD_NUMBER: version upgraded for chaos-charts"
+  git commit --message " $GITHUB_RUN_ID: version upgraded for chaos-charts"
   git status
-}
-
-upload_files() {
-  git remote -v
-  git push origin master
 }
 
 setup_git
 commit_updated_changes
-upload_files
+
